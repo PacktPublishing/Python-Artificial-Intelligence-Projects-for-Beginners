34711dfd72aeb66cfe914fffda1db3ee0328c8bf
Rolly Maulana Awangga <rolly@awang.ga>
2019-04-09 07:16:57 +0700

    update code

--- Chapter04/SpamDetectorNN.py
+++ Chapter04/SpamDetectorNN.py
@@ -1,3 +1,4 @@
+# In[1]: import library yang diperlukan
 import pandas as pd
 from keras.preprocessing.text import Tokenizer
 import numpy as np
@@ -6,12 +7,28 @@ from keras.layers import Dense, Dropout, Activation
 from keras.utils import np_utils
 from sklearn.model_selection import StratifiedKFold
 
+# In[2]: tumpuk 5 data csv kedalam variabel d terus kocok2
+
 d = pd.concat([pd.read_csv("Youtube01-Psy.csv"),
                pd.read_csv("Youtube02-KatyPerry.csv"),
                pd.read_csv("Youtube03-LMFAO.csv"),
                pd.read_csv("Youtube04-Eminem.csv"),
                pd.read_csv("Youtube05-Shakira.csv")])
 d = d.sample(frac=1)
+
+# In[3]: mengecek k fold cross validation
+    
+kfold = StratifiedKFold(n_splits=5)
+splits = kfold.split(d, d['CLASS'])
+
+# In[3]: mengecek k fold cross validation
+for train, test in splits:
+    print("--------------Splits---------------------")
+    print(test)
+
+
+
+# In[3]: membuat fungsi train dan test
 
 def train_and_test(train_idx, test_idx):
     
@@ -55,12 +72,16 @@ def train_and_test(train_idx, test_idx):
     print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
     return scores
 
+
+
+# In[4]: mengecek k fold cross validation
+    
 kfold = StratifiedKFold(n_splits=5)
 splits = kfold.split(d, d['CLASS'])
 cvscores = []
 for train_idx, test_idx, in splits:
     scores = train_and_test(train_idx, test_idx)
     cvscores.append(scores[1] * 100)
-
+# In[4]: print rata2 dari skore akurasi
 print("%.2f%% (+/- %.2f%%)" % (np.mean(cvscores), np.std(cvscores)))
 
