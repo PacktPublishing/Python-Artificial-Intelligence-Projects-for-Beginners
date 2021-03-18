6e3e866a629e3f071459ff10ea93b4fac9587452
Ahmad Agung Tawakkal <51843028+ahmadagungtawakkal30@users.noreply.github.com>
2021-03-18 23:00:19 +0700

    Update AhmadAgung1184015.py

--- Chapter01/AhmadAgung1184015.py
+++ Chapter01/AhmadAgung1184015.py
@@ -5,42 +5,26 @@ Created on Thu Mar 18 22:32:24 2021
 @author: ahmad
 """
 
-import pandas as pd
+import pandas
+from sklearn import tree
+from sklearn.model_selection import cross_val_score
 
-def preparation(datasetpath):
-    d = pd.read_csv(datasetpath, sep=',')
-    len(d)
-    
-    # generate binary label for attribute 'Revenue'
-    d['ots'] = d.apply(lambda row: 1 if (row['Other_Sales']) == 'TRUE' else 0, axis=1)
-    d.head()
-    
-    #shuffle data
-    d_shuffle = d.sample(frac=1)
-        
-    # data training 80%
-    percent_training = int(len(d)*0.80)
-    d_train = d_shuffle[:percent_training]
-    d_train_pass = d_train['ots'] #label
-    d_train_att = d_train.drop(['ots'], axis=1) #fitur
-        
-    # data testing 20%
-    d_test = d_shuffle[percent_training:]
-    d_test_pass = d_test['ots']
-    d_test_att = d_test.drop(['ots'], axis=1)
-        
-        
-    d_att = d_shuffle.drop(['ots'], axis=1)
-    d_pass = d_shuffle['ots']
-    
-    return d_train_att,d_train_pass,d_test_att,d_test_pass,d_att,d_pass
+def preparation():
+    vg = pandas.read_csv('Chapter01/dataset/vgsales.csv', sep=',')
+    vg = vg.sample(frac=1)
+    vg_train = vg[:5147]
+    vg_test = vg[5147:]
+    vg_train_attribute = vg_train.drop(['gbs'], axis=1)
+    vg_train_gbs = vg_train['gbs']
+    vg_test_attribute = vg_test.drop(['gbs'], axis=1)
+    vg_test_gbs = vg_test['gbs']
+    data = [[vg_train_attribute,vg_train_gbs], [vg_test_attribute, vg_test_gbs]]
+    return data
 
-def training(d_train_att,d_train_pass):
-    # fit a decision tree
-    from sklearn import tree
-    s = tree.DecisionTreeClassifier(criterion="entropy", max_depth=5)
-    s = s.fit(d_train_att, d_train_pass)
-    return s
+def training(vg_train_att, vg_train_gbs):
+    t = tree.DecisionTreeClassifier(criterion="entropy", max_depth=5)
+    t = t.fit(vg_train_att,vg_train_gbs)
+    return t
 
-def testing(s,testdataframe):
-    return s.predict(testdataframe)
+def testing(t, testdataframe):
+    return t.predict(testdataframe)
