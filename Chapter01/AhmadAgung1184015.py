98b70aef01d2afbcb8c868e8661af36131786bfb
Ahmad Agung Tawakkal <51843028+ahmadagungtawakkal30@users.noreply.github.com>
2021-03-18 22:19:27 +0700

    Update AhmadAgung1184015.py

--- Chapter01/AhmadAgung1184015.py
+++ Chapter01/AhmadAgung1184015.py
@@ -4,20 +4,20 @@ def preparation(datasetpath):
     d = pd.read_csv(datasetpath, sep=',')
     len(d)
     
-    d['GS'] = d.apply(lambda row: 1 if (row['Global_Sales']) == 'TRUE' else 0, axis=1)
+    d['gbs'] = d.apply(lambda row: 1 if (row['Global_Sales']) == 'TRUE' else 0, axis=1)
     d.head()
 
     d_shuffle = d.sample(frac=1)
 
     percent_training = int(len(d)*0.80)
     d_train = d_shuffle[:percent_training]
-    d_train_pass = d_train['GS'] 
-    d_train_att = d_train.drop(['GS'], axis=1)       
+    d_train_pass = d_train['gbs'] 
+    d_train_att = d_train.drop(['gbs'], axis=1)       
     d_test = d_shuffle[percent_training:]
-    d_test_pass = d_test['GS']
-    d_test_att = d_test.drop(['GS'], axis=1)       
-    d_att = d_shuffle.drop(['GS'], axis=1)
-    d_pass = d_shuffle['GS']
+    d_test_pass = d_test['gbs']
+    d_test_att = d_test.drop(['gbs'], axis=1)       
+    d_att = d_shuffle.drop(['gbs'], axis=1)
+    d_pass = d_shuffle['gbs']
     
     return d_train_att,d_train_pass,d_test_att,d_test_pass,d_att,d_pass
 
