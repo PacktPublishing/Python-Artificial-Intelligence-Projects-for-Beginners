0ea0b90ae3ef4b7d5865762c1a349aaf712c10c8
Ahmad Agung Tawakkal <51843028+ahmadagungtawakkal30@users.noreply.github.com>
2021-03-18 22:34:46 +0700

    Update AhmadAgung1184015.py

--- Chapter01/AhmadAgung1184015.py
+++ Chapter01/AhmadAgung1184015.py
@@ -1,31 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 18 22:32:24 2021
+
+@author: ahmad
+"""
+
 import pandas as pd
 
 def preparation(datasetpath):
     d = pd.read_csv(datasetpath, sep=',')
     len(d)
     
-    d['gbs'] = d.apply(lambda row: 1 if (row['Global_Sales']) == 'TRUE' else 0, axis=1)
+    # generate binary label for attribute 'Revenue'
+    d['ots'] = d.apply(lambda row: 1 if (row['Other_Sales']) == 'TRUE' else 0, axis=1)
     d.head()
-
+    
+    #shuffle data
     d_shuffle = d.sample(frac=1)
-
+        
+    # data training 80%
     percent_training = int(len(d)*0.80)
     d_train = d_shuffle[:percent_training]
-    d_train_pass = d_train['gbs'] 
-    d_train_att = d_train.drop(['gbs'], axis=1)       
+    d_train_pass = d_train['ots'] #label
+    d_train_att = d_train.drop(['ots'], axis=1) #fitur
+        
+    # data testing 20%
     d_test = d_shuffle[percent_training:]
-    d_test_pass = d_test['gbs']
-    d_test_att = d_test.drop(['gbs'], axis=1)       
-    d_att = d_shuffle.drop(['gbs'], axis=1)
-    d_pass = d_shuffle['gbs']
+    d_test_pass = d_test['ots']
+    d_test_att = d_test.drop(['ots'], axis=1)
+        
+        
+    d_att = d_shuffle.drop(['ots'], axis=1)
+    d_pass = d_shuffle['ots']
     
     return d_train_att,d_train_pass,d_test_att,d_test_pass,d_att,d_pass
 
 def training(d_train_att,d_train_pass):
+    # fit a decision tree
     from sklearn import tree
-    t = tree.DecisionTreeClassifier(criterion="entropy", max_depth=5)
-    t = t.fit(d_train_att, d_train_pass)
-    return t
+    s = tree.DecisionTreeClassifier(criterion="entropy", max_depth=5)
+    s = s.fit(d_train_att, d_train_pass)
+    return s
 
-def testing(t,testdataframee):
-    return t.predict(testdataframee)
+def testing(s,testdataframe):
+    return s.predict(testdataframe)
