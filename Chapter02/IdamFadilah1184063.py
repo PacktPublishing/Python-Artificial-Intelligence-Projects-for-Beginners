eee86fdb49ed06bf4e09ec34dec2a7b2e0b9268e
idam fadilah <idamfadilah.9c@gmail.com>
2021-04-01 08:49:38 +0700

    bismillah

--- Chapter02/IdamFadilah1184063.py
+++ Chapter02/IdamFadilah1184063.py
@@ -2,17 +2,24 @@ import pandas as pd
 from sklearn.preprocessing import OrdinalEncoder
 from sklearn.ensemble import RandomForestClassifier
 
+import pandas as pd
+from sklearn.preprocessing import OrdinalEncoder, OneHotEncoder
+from sklearn.ensemble import RandomForestClassifier
+
+
 def preparation():
     ord = OrdinalEncoder()
-    data = pd.read_csv('Chapter01/dataset/toy_dataset.csv')
+    data = pd.read_csv('Chapter01/dataset/toys.txt', sep=',', usecols=[1,2,3,4,5], header=None, names=['index', 'City', 'Gender', 'Age', 'Income', 'Illness'])
     data["Gender"] = ord.fit_transform(data[["Gender"]])
     data["City"] = ord.fit_transform(data[["City"]])
     data["Illness"] = ord.fit_transform(data[["Illness"]])
-    data = data.iloc[:, 1:]
     data = data.sample(frac=1)
-    data = [data.iloc[:,:4], data.iloc[:,4:]]
+    data = [data.iloc[:,:4], data.iloc[:, 4:]]
+
+
     dataAttr = data.pop(0)
     dataVar = data.pop(0)
+    
 
     length = int(len(dataVar)*0.75)
 
@@ -25,7 +32,7 @@ def preparation():
     return [[trainAttr, trainVar], [testAttr, testVar]]
 
 def training(trainAttr, trainVar):
-    t = RandomForestClassifier(random_state = 0, n_estimators=100)
+    t = RandomForestClassifier(max_features=4, random_state=0, n_estimators=100)
     t = t.fit(trainAttr, trainVar)
     return t
 
